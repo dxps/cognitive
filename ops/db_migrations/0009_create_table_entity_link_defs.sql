ffcca85ff33ab2940314d7b448de4fdd90af46b7
dxps <marius.ileana@gmail.com>
2024-07-17 18:05:37 +0300

    feat(physical data model): add entity links

---
+++ ops/db_migrations/0009_create_table_entity_link_defs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE entity_link_defs
+(
+    id                      CHAR(10)         PRIMARY KEY,
+    name                    VARCHAR(32),
+    description             VARCHAR(128),
+    arity                   VARCHAR(32),
+    entity_def_source_id    CHAR(10)         NOT NULL,
+    entity_def_target_id    CHAR(10)         NOT NULL,
+    CONSTRAINT entity_def_source_fk FOREIGN KEY(entity_def_source_id) REFERENCES entity_defs(id),
+    CONSTRAINT entity_def_target_fk FOREIGN KEY(entity_def_target_id) REFERENCES entity_defs(id)
+);
