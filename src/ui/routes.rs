322820f5e50bee4dfe23eddaff64d207058363dc
dxps <marius.ileana@gmail.com>
2024-10-17 17:20:37 +0300

    chore: remove Home from breadcrumb

--- src/ui/routes.rs
+++ src/ui/routes.rs
@@ -64,51 +64,36 @@ pub enum Route {
 impl Route {
     pub fn get_path(to: Route) -> Vec<(String, Route)> {
         match to {
-            Route::Home {} => vec![("Home".into(), Route::Home {})],
-            Route::Login {} => vec![("Home".into(), to), ("Login".into(), Route::Login {})],
-            Route::Logout {} => vec![("Home".into(), Route::Home {}), ("Logout".into(), to)],
-            Route::UserProfile { username: _ } => vec![("Home".into(), Route::Home {}), ("User Profile".into(), to)],
-            Route::Admin {} => vec![("Home".into(), Route::Home {}), ("Admin".into(), to)],
-            Route::AttributeDefListPage {} => vec![
-                ("Home".into(), Route::Home {}),
-                ("Admin".into(), Route::Admin {}),
-                ("Attributes Definitions".into(), to),
-            ],
+            Route::Login {} => vec![("Login".into(), Route::Login {})],
+            Route::Logout {} => vec![("Logout".into(), to)],
+            Route::UserProfile { username: _ } => vec![("User Profile".into(), to)],
+            Route::Admin {} => vec![("Admin".into(), to)],
+            Route::AttributeDefListPage {} => vec![("Admin".into(), Route::Admin {}), ("Attributes Definitions".into(), to)],
             Route::AttributeDefNewPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Attributes Definitions".into(), Route::AttributeDefListPage {}),
                 ("New".into(), to),
             ],
             Route::EntityDefListPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Entities Definitions".into(), Route::EntityDefListPage {}),
             ],
             Route::EntityDefNewPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Entities Definitions".into(), Route::EntityDefListPage {}),
                 ("New".into(), to),
             ],
             Route::EntityListPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Entities".into(), Route::EntityListPage {}),
             ],
             Route::EntityNewPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Entities".into(), Route::EntityListPage {}),
                 ("New".into(), to),
             ],
-            Route::TagListPage {} => vec![
-                ("Home".into(), Route::Home {}),
-                ("Admin".into(), Route::Admin {}),
-                ("Tags".into(), to),
-            ],
+            Route::TagListPage {} => vec![("Admin".into(), Route::Admin {}), ("Tags".into(), to)],
             Route::TagNewPage {} => vec![
-                ("Home".into(), Route::Home {}),
                 ("Admin".into(), Route::Admin {}),
                 ("Tags".into(), Route::TagListPage {}),
                 ("New".into(), to),
@@ -118,13 +103,12 @@ impl Route {
                 let tag_name = format!("id:{}", id);
                 Route::get_path_to_tag(to, tag_name)
             }
-            _ => vec![("Home".into(), Route::Home {}), ("Admin".into(), Route::Admin {})],
+            _ => vec![("Admin".into(), Route::Admin {})],
         }
     }
 
     pub fn get_path_to_tag(to: Route, tag_name: String) -> Vec<(String, Route)> {
         vec![
-            ("Home".into(), Route::Home {}),
             ("Admin".into(), Route::Admin {}),
             ("Tags".into(), Route::TagListPage {}),
             (tag_name, to),
@@ -133,7 +117,6 @@ impl Route {
 
     pub fn get_path_to_attr_def(to: Route, attr_def_name: String) -> Vec<(String, Route)> {
         vec![
-            ("Home".into(), Route::Home {}),
             ("Admin".into(), Route::Admin {}),
             ("Attributes Definitions".into(), Route::AttributeDefListPage {}),
             (attr_def_name, to),
@@ -142,7 +125,6 @@ impl Route {
 
     pub fn get_path_to_ent_def(to: Route, ent_def_name: String) -> Vec<(String, Route)> {
         vec![
-            ("Home".into(), Route::Home {}),
             ("Admin".into(), Route::Admin {}),
             ("Entities Definitions".into(), Route::EntityDefListPage {}),
             (ent_def_name, to),
@@ -151,7 +133,6 @@ impl Route {
 
     pub fn get_path_to_ent(to: Route, ent_def_name: String) -> Vec<(String, Route)> {
         vec![
-            ("Home".into(), Route::Home {}),
             ("Admin".into(), Route::Admin {}),
             ("Entities".into(), Route::EntityListPage {}),
             (ent_def_name, to),
