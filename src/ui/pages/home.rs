56462c463f8c50643b8dc8f35fb9de0699a30918
dxps <marius.ileana@gmail.com>
2024-10-26 01:16:49 +0300

    style: akasha svg at homepage

--- src/ui/pages/home.rs
+++ src/ui/pages/home.rs
@@ -9,7 +9,10 @@ pub fn Home() -> Element {
         div { class: "flex flex-col min-h-screen bg-gray-100",
             Nav {}
             div { class: "flex flex-col min-h-screen justify-center items-center drop-shadow-2xl",
-                img { class: "h-16 w-16 logo_filter", src: "/akasha.svg" }
+                img {
+                    class: "w-[86px] h-[86px] logo_filter",
+                    src: "/akasha.svg"
+                }
                 p { class: "text-4xl font-bold text-gray-300", "Akasha" }
                 p { class: "text-sm text-gray-500", "A space of knowledge." }
             }
