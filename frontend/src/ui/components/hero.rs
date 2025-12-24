21b813b0478bcf9270a4fdfe42f20bb10cb35ee3
dxps <marius.ileana@gmail.com>
2025-12-24 20:18:06 +0200

    chore(fe): logo favicon(.ico) and png

--- frontend/src/ui/components/hero.rs
+++ frontend/src/ui/components/hero.rs
@@ -1,7 +1,5 @@
 use dioxus::prelude::*;
 
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-
 #[component]
 pub fn Hero() -> Element {
     rsx! {
@@ -10,14 +8,15 @@ pub fn Hero() -> Element {
             // Attributes should be defined in the element before any children
             id: "hero",
             // After all attributes are defined, we can define child elements and components
-            img { src: HEADER_SVG, id: "header" }
             div { id: "links",
                 // The RSX macro also supports text nodes surrounded by quotes
                 a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
                 a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
                 a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
                 a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                    "💫 VSCode Extension"
+                }
                 a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
             }
         }
